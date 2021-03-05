bcf0ecbacfe52f6a5d2c41e2325d40b5d4c804ad
Brandon Knox <bknox.dev@gmail.com>
2021-03-05 09:11:29 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Brandon Knox on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
