9324077c953345a8bf2d789950d09a5004b38f89
Brandon Knox <bknox.dev@gmail.com>
2021-03-05 10:08:27 -0700

    4 steps needed to bridge swift text format to objective c format for spell checking

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -10,15 +10,11 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        let input = """
-                    a
-                    b
-                    c
-                    """
-        let letters = input.components(separatedBy: "\n")
-        let letter = letters.randomElement()
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
-        
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
         
         return Text("Hello World")
     }
